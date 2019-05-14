16769a1898b7725157780d8bad112b78218dc115
Sagar Andre <Sagar.Andre@asos.com>
2019-05-14 06:48:13 +0100

    modified

--- src/Components/RecipeDetails.js
+++ src/Components/RecipeDetails.js
@@ -22,16 +22,22 @@ class RecipeDetails extends Component{
     {
         
        const {setvisibility} = this.props;
-const {name,ingredients,author} = this.state.recipe[0];
+const {name,ingredients,author,steps} = this.state.recipe[0];
 
 
         return(
             <React.Fragment>
               <div>
                 <h1>Recipe details</h1>
-                <h2>{name}</h2>
-                Author:- <h2>{author} </h2>
-                Ingredients :- <h3>{ingredients}</h3>
+                <h2>{name}</h2> < br/>
+                <h2> Author</h2> 
+                {author} < br />  < br />
+                <h2>  Ingredients</h2> 
+               {ingredients} < br />  < br />
+               <h2>  Steps</h2> 
+              
+                  {steps}
+                < br />  < br />
             <button type="button" className="btn btn-warning" onClick={() => setvisibility(0)} >Go Back</button>
             </div>
             </React.Fragment>
--- src/Components/tempdetails.js
+++ src/Components/tempdetails.js
@@ -2,29 +2,33 @@ export const tempdetails = [
     {
         id:1,
         name:"chiken tikka",
-        ingredients: "chiken , garam masala, butter",
+        ingredients: "1 kilograms chicken, 1 tablespoon lime juice, 2 tablespoon yoghurt (curd), 2 teaspoon Red chilli powder, 1 teaspoon coriander powder, 2 teaspoon garlic paste, 2 teaspoon ginger paste, 2 teaspoon chaat masala",
         author:"sagar andre",
+        steps:"Cut the chicken into 1-inch cubes. In a large bowl, mix chicken and the other ingredients well and marinate it for 3 hours or overnight.Pierce the chicken cubes on a skewer and turn over a fire or under grill for 10 mins, cooking both the sides.Serve it hot, garnished with preferred seasoning and lime juice."
         
     },
     {
         id:2,
         name:"paneer tikka",
-        ingredients: "paneer , garam masala, butter",
+        ingredients: "200 gm cubed paneer200 gm cubed paneer,1/8 teaspoon edible food color, 2 pinch salt, 1/2 cup chopped onion, 1 inch ginger, 6 pieces garlic, 1 teaspoon pureed cashews, 1 cup tomato puree, 1 tablespoon gram flour (besan), 3 tablespoon refined oil, 1/2 cup yoghurt (curd), 1 tablespoon cumin seeds, 6 red chilli, 6 peppercorns",
         author:"nikhil ligade",
+        steps:"Here’s how you can prepare this paneer recipe at home with some easily available ingredients. The very first step is to prepare the Paneer Tikka and for that you need to prepare a marination mixture by mixing the ginger paste, garlic paste, chat masala, lime juice, cumin powder and salt. Let it rest for a minute and coat the cubed paneer pieces with mixture and let it marinade for about 30 minutes.At 200 degree C bake the marinated paneer pieces for 10 minutes. Paneer Tikka is prepared and you can keep it aside in a bowl. Now, to prepare the gravy, heat 2 tablespoons of oil and fry the onion till they turn brown and translucent. While the onions are done, let it cool.Grind them well and keep them aside. Then mix ginger, garlic, cashew nuts, cumin, poppy seeds, dry red chillies and peppercorn in the grinder and grind them into a smooth thick paste. Take a pan and heat it on low-medium flame, heat 4 tablespoon of oil and stir-fry the finely grinded mixture till it turns brown and add gram flour. Roast the flour for a minute and then add the curd and cook it for about 5 minutes.Now, add tomato puree and cook till the puree dries up. Then add 3 cups of hot water and a pinch of tandoori edible food colour. Mix them all together and let the gravy cook for about 2-4 minutes. When it's cooked, simmer it for another minute and reduce the flame.For preparing the Paneer Tikka Masala, add the baked paneer tikka in the gravy and cook it till the gravy is thick enough. When your paneer dish is cooked, switch off the burner. Empty this sumptuous dish into a large serving bowl and garnish it with coriander leaves and cream. And serve this rich creamy recipe with your choice of Indian bread or Rice."
         
     },
     {
         id:3,
         name:"chiken chilly",
-        ingredients: "chiken , soya sauce",
+        ingredients: "400 gm chopped chicken, 1 beaten egg, 1/2 cup powdered corn flour, 1/2 teaspoon ginger paste, 1/2 teaspoon garlic paste, 2 cup chopped onion, 2 tablespoon sliced green chilli, 1 tablespoon soy sauce, 2 tablespoon vinegar, 1 handful chopped spring onions, 1/2 cup sunflower oil, 1/2 teaspoon powdered salt",
         author:"vishal deshmukh",
+        steps:"To make this easy chicken recipe, combine chicken, eggs, ginger garlic paste and corn flour in a bowl. Mix all the ingredients properly. Then in the same bowl, pour enough water so that the chicken pieces are well coated with the batter. Once done, keep aside the batter in a refrigerator to marinate for 1-2 hours.After 1-2 hours, take out the marinade. Next, heat oil in a deep pan over high flame. Carefully place the chicken pieces in the oil until cooked through. Remove and drain excess oil on an absorbent paper. Meanwhile, heat 1 or 2 tbsp of oil in a separate pan over moderate flame. Once the oil is sufficiently hot, add the onions and fry for 2-3 minutes till they turn translucent. Then add the green chilies and mix well again. You can also add some dry roasted green chilies to accentuate the taste. Moreover, If you like it very spicy, you can also add some vinegar soaked green chilies to this recipe, as it adds on to the taste and flavor.After a while, add soy sauce, vinegar, chicken and salt. Mix all the ingredients well and make sure that the chicken is well coated with the sauces.Once done, remove and transfer the dish to a bowl. Garnish with the spring onions, roasted sesame and serve it to your family and friends. We are sure your loved ones will love this awesome recipe."
         
     },
     {
         id:4,
         name:"paneer chilly",
-        ingredients: "paneer , soya sauce",
+        ingredients: "200 gm paneer, 1/2 small ginger, 1 tablespoon soy sauce, 1 clove garlic, 2 tablespoon tomato ketchup, 3 medium green chilli,1/2 teaspoon salt, 1 tablespoon green chilli sauce, 1 large capsicum (green pepper), 1 tablespoon vinegar, 1 large onion, 1/2 cup refined oil",
         author:"yogesh jadhav",
+        steps:"To prepare this yummy appetizer recipe, take a chopping board and chop the paneer, onion and capsicum. Make sure that they are cut in cubes. Wash the cubed capsicums in running water and keep the cubed veggies aside. Now, peel ginger & garlic and chop them in a small bowl. Then cut the green chillies in strips or you can also slice them half. Next, put a skillet on medium flame and heat oil in it. When the oil is sufficiently hot, add the cubed paneer pieces in it and fry them till they turn golden. Now, put a saucepan on another burner and add two tablespoons of hot oil in it. Immediately, add the chopped ginger and garlic along with slit chillies. Let them saute for a few seconds.  After sauteing them, add the cubed onions in the saucepan. Saute for a minute and then add cubed capsicum, salt along with the tomato ketchup, vinegar, green chilli sauce, and soy sauce. Let the veggies cook for two to three minutes. Finally, add the fried paneer cubes and toss them once. Garnish with chopped coriander leaves and enjoy hot!"
         
     },
 ]
