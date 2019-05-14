c0bddab939dcbafd5ebd270210dd4239d41ab936
Sagar Andre <Sagar.Andre@asos.com>
2019-05-14 07:04:16 +0100

    Modified data

--- src/Components/RecipeDetails.js
+++ src/Components/RecipeDetails.js
@@ -35,8 +35,13 @@ const {name,ingredients,author,steps} = this.state.recipe[0];
                 <h2>  Ingredients</h2> 
                {ingredients} < br />  < br />
                <h2>  Steps</h2> 
-              
-                  {steps}
+               <ul>
+                 {steps.map((step) => <li>
+                   {step}
+                 </li>)
+                }
+               </ul>
+                
                 < br />  < br />
             <button type="button" className="btn btn-warning" onClick={() => setvisibility(0)} >Go Back</button>
             </div>
