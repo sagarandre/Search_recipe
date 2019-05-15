32740733c18820d8bc9d08ac973bdfdd8272b468
Sagar Andre <Sagar.Andre@asos.com>
2019-05-15 07:54:24 +0100

    Modified

--- src/Components/RecipeDetails.js
+++ src/Components/RecipeDetails.js
@@ -22,28 +22,33 @@ class RecipeDetails extends Component{
     {
         
        const {setvisibility} = this.props;
-const {name,ingredients,author,steps} = this.state.recipe[0];
+const {name,image_url,ingredients,steps} = this.state.recipe[0];
 
 
         return(
             <React.Fragment>
-              <div>
+              <div className="row">
+              <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12">
+                <br /> <br />
+              <img src={image_url} alt={name} height="400px" width="400px" />
+              </div>
+              <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12">
                 <h1>Recipe details</h1>
                 <h2>{name}</h2> < br/>
-                <h2> Author</h2> 
-                {author} < br />  < br />
+                
                 <h2>  Ingredients</h2> 
                {ingredients} < br />  < br />
                <h2>  Steps</h2> 
                <ul>
-                 {steps.map((step) => <li>
+                 {steps.map((step) => <li key={step.id}>
                    {step}
                  </li>)
                 }
                </ul>
                 
-                < br />  < br />
+                < br />  
             <button type="button" className="btn btn-warning" onClick={() => setvisibility(0)} >Go Back</button>
+            </div>
             </div>
             </React.Fragment>
         )
